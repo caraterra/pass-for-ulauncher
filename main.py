c1a1ff51ba68b58e4cec0ca347b53cd1a6c618bb
Alexander J Carter <acarter0077@gmail.com>
2021-10-21 20:14:13 -0400

    Got started

---
+++ main.py
@@ -0,0 +1,26 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+
+class DemoExtension(Extension):
+    def __init__(self):
+        super().__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        items = []
+        for i in range(5):
+            items.append(ExtensionResultItem(icon='images/file-lock.png',
+                                             name='Password %s' % i,
+                                             description='This is password #%s' % i,
+                                             on_enter=HideWindowAction()))
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    DemoExtension().run()
