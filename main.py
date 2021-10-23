20935f60c10c5762f5f0e1dfd73696124e4d0b66
Alexander J Carter <acarter0077@gmail.com>
2021-10-23 02:08:19 -0400

    Got password files to display. Still need to work on item search and cropping list length.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-from pathlib import Path
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+from pathlib import *
 from os import getenv
 
 prefix = None
@@ -13,18 +14,26 @@ class PassExtension(Extension):
     def __init__(self):
         super().__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
+# Runs when the text in the prompt is changed
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
+        password_files = list(prefix.rglob('*{}*.gpg'.format(event.get_argument())))
         items = []
-        # for i in range(5):
-        #     items.append(ExtensionResultItem(icon='images/file-earmark-lock.svg',
-        #                                      name='Password %s' % i,
-        #                                      description='This is password #%s' % i,
-        #                                      on_enter=HideWindowAction()))
+        print(event.get_argument())
+        for i in password_files:
+            items.append(ExtensionResultItem(icon='images/file-earmark-lock.svg',
+                                             name='{}'.format(i.stem),
+                                             description='', # Put filepath here eventually
+                                             on_enter=HideWindowAction()))
         return RenderResultListAction(items)
 
-if __name__ == '__main__':
-    prefix = Path(getenv('PASSWORD_STORE_DIR', '~/.password-store'))
+# Runs when the user submits an item
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        pass # I'll need this later
 
+if __name__ == '__main__':
+    prefix = Path(getenv('PASSWORD_STORE_DIR', '~/.password-store')).expanduser()
     PassExtension().run()
