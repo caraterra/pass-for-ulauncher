bec6a391676049a55e7401e62849e03be1aa868d
Alexander J Carter <acarter0077@gmail.com>
2021-10-23 19:21:58 -0400

    Implemented item search, cropping list size, and copying password selection to clipboard

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
-from pathlib import *
-from os import getenv
+from pathlib import Path
+import os
 
 prefix = None
+# TODO: Make Maximum visible options a user option
+MAX_VIS_OPTIONS=8
+
+def sort_by_basename(fname):
+    return fname.name
 
 class PassExtension(Extension):
     def __init__(self):
@@ -19,21 +24,31 @@ class PassExtension(Extension):
 # Runs when the text in the prompt is changed
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
-        password_files = list(prefix.rglob('*{}*.gpg'.format(event.get_argument())))
+        search_str = event.get_argument()
+        if search_str:
+            password_files = sorted(list(prefix.rglob(f'*{search_str}*.gpg')), key=sort_by_basename)
+        else:
+            password_files = sorted(list(prefix.rglob('*.gpg')), key=sort_by_basename)
+
         items = []
-        print(event.get_argument())
-        for i in password_files:
+        for pfile in password_files:
             items.append(ExtensionResultItem(icon='images/file-earmark-lock.svg',
-                                             name='{}'.format(i.stem),
-                                             description='', # Put filepath here eventually
-                                             on_enter=HideWindowAction()))
+                                             name=f'{pfile.stem}',
+                                             description=f'{pfile}'.replace(str(Path.home()), "~"),
+                                             on_enter=ExtensionCustomAction(pfile)))
+            if (len(items) >= MAX_VIS_OPTIONS):
+                break
+
         return RenderResultListAction(items)
 
 # Runs when the user submits an item
 class ItemEnterEventListener(EventListener):
     def on_event(self, event, extension):
-        pass # I'll need this later
+        # TODO: notify the user that the password has been copied
+        pass_arg = str(event.get_data().relative_to(prefix)).replace('.gpg', '')
+        pass_cmd = f'pass show -c {pass_arg} > /dev/null'.format(pass_arg)
+        os.system(pass_cmd)
 
 if __name__ == '__main__':
-    prefix = Path(getenv('PASSWORD_STORE_DIR', '~/.password-store')).expanduser()
+    prefix = Path(os.getenv('PASSWORD_STORE_DIR', '~/.password-store')).expanduser()
     PassExtension().run()
