458962a4c8137e2f901d93037b1bde301eb15158
Alexander J Carter <acarter0077@gmail.com>
2021-12-05 02:03:42 -0500

    Add desktop notifications on password copy

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
+"""Used for getting the user's PASSWORD_STORE_DIR env var"""
+from os import getenv, system
+from pathlib import Path
+from gi.repository import Notify, GdkPixbuf
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+# from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
-from pathlib import Path
-import os
 
 prefix = ""
 
 
 def sort_by_basename(fname):
+    """Takes Path objects, and returns their name so they may be sorted by list()"""
     return fname.name
 
+
 class PassExtension(Extension):
+    """Initializes the extension"""
+
     def __init__(self):
         super().__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
-# Runs when the text in the prompt is changed
+
 class KeywordQueryEventListener(EventListener):
+    """Runs when the text in the prompt is changed"""
+
     def on_event(self, event, extension):
         search_str = event.get_argument()
         if search_str:
-            password_files = sorted(list(prefix.rglob(f'*{search_str}*.gpg')), key=sort_by_basename)
+            password_files = sorted(
+                list(prefix.rglob(f"*{search_str}*.gpg")), key=sort_by_basename
+            )
         else:
-            password_files = sorted(list(prefix.rglob('*.gpg')), key=sort_by_basename)
+            password_files = sorted(list(prefix.rglob("*.gpg")), key=sort_by_basename)
 
         items = []
         for pfile in password_files:
@@ -44,14 +55,23 @@ class KeywordQueryEventListener(EventListener):
 
         return RenderResultListAction(items)
 
-# Runs when the user submits an item
-class ItemEnterEventListener(EventListener):
-    def on_event(self, event, extension):
-        # TODO: notify the user that the password has been copied
-        pass_arg = str(event.get_data().relative_to(prefix)).replace('.gpg', '')
-        pass_cmd = f'pass show -c {pass_arg} > /dev/null'.format(pass_arg)
-        os.system(pass_cmd)
 
-if __name__ == '__main__':
-    prefix = Path(os.getenv('PASSWORD_STORE_DIR', '~/.password-store')).expanduser()
+class ItemEnterEventListener(EventListener):
+    """Runs when the user submits an item"""
+
+    def on_event(self, event, extension):
+        pass_arg = str(event.get_data().relative_to(prefix)).replace(".gpg", "")
+        pass_cmd = f"pass show -c {pass_arg} > /dev/null".format(pass_arg)
+        system(pass_cmd)
+        Notify.Notification.new(
+            f"Copied {pass_arg} to clipboard.",
+            "Will clear in 45 seconds.",
+            "object-unlocked",
+        ).show()
+
+
+if __name__ == "__main__":
+    prefix = Path(getenv("PASSWORD_STORE_DIR", "~/.password-store")).expanduser()
+    Notify.init("Pass for Ulauncher")
     PassExtension().run()
+    Notify.uninit()
